20fd8ae94a299365f1e0da0f9d512911d29a2783
Thi <dinhquangthi195@gmail.com>
2019-08-25 10:18:11 +0700

    finish them-sua-xoa san pham

--- admin/modules/product/delete.php
+++ admin/modules/product/delete.php
@@ -1,30 +1,29 @@
 <?php 
-    $open = "category";
+    $open = "product";
     require_once __DIR__. "/../../autoload/autoload.php";
       
     
     $id = intval(getInput('id'));
 
-    $EditCategory = $db->fetchID("category",$id);
-    if(empty($EditCategory))
+
+    $EditProduct = $db->fetchID("product",$id);
+    if(empty($EditProduct))
     {
         $_SESSION['error'] = "Dữ liệu không tồn tại";
-        redirectAdmin("category");
+        redirectAdmin("product");
     }
     
-    $num = $db->delete("category",$id);
+    $num = $db->delete("product",$id);
     if($num > 0) 
     {
         $_SESSION['success'] = "Xóa thành công";
-          redirectAdmin("category");
+          redirectAdmin("product");
     }
     else
      {
-          
              $_SESSION['error'] = " Xóa thất bại";
-              redirectAdmin("category");
+              redirectAdmin("product");
         }
-
     
 ?>
 
