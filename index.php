32a7bd92107ae6ffae7af77bd1f87c33de0b0f5f
Thi <dinhquangthi195@gmail.com>
2019-09-02 21:49:56 +0700

    update nut next & prev phan trang

--- index.php
+++ index.php
@@ -43,6 +43,13 @@
 <?php require_once __DIR__. "/user/layouts/header.php"; ?>
 <?php require_once __DIR__. "/user/layouts/banner.php"; ?>
 
+<div class="top-page">
+    <div class="top-page__content"><i class="fas fa-truck-moving"></i>MIỄN PHÍ 3 NGÀY ĐỔI HÀNG</div>
+    <div class="top-page__content"><i class="fas fa-exchange-alt"></i>ĐỔI TRẢ TRONG VÒNG 7 NGÀY</div>
+    <div class="top-page__content"><i class="fas fa-dolly"></i>MIỄN PHÍ GIAO HÀNG NỘI THÀNH</div>
+    <div class="top-page__content"><i class="far fa-money-bill-alt"></i>THANH TOÁN KHI NHẬN HÀNG</div>
+</div>
+
 <section>
     <div class="container">
         <div class="row list-product">
@@ -124,7 +131,7 @@
                         <?php endif ?>
 
 
-                        <a href="add-cart.php?id=<?php echo $value['id'] ?>"><button class="add-cart">XEM CHI TIẾT</button></a>
+                        <a href="<?php echo url_home()?>/user/details.php?id=<?php echo $value['id'] ?>"><button class="add-cart">XEM CHI TIẾT</button></a>
                     </div>
                 
                     <?php endforeach ?>
@@ -132,20 +139,17 @@
                 
                 <nav aria-label="Page navigation example ">
                     <ul class="pagination justify-content-center">
-                        <li class="page-item">
-                            <a class="page-link pagi" href="" aria-label="Previous">
-                                <span aria-hidden="true">&laquo;</span>
-                            </a>
-                        </li>
-                        <?php for($i=1; $i <= $sotrang; $i++): ?>
-                        <li class="page-item <?php echo ($i == $p) ? 'active' : '' ?>">
-                        <a class="page-link pagi" href="?page=<?php echo $i ;?>"><?php echo $i; ?></a></li>
-                        <?php endfor ?>
-                        <li class="page-item">
-                            <a class="page-link pagi" href="" aria-label="Next">
-                                <span aria-hidden="true">&raquo;</span>
-                            </a>
-                        </li>
+                      
+                        <a class="page-link pagi nutprev" href="" aria-label="Previous">
+                             <span aria-hidden="true">&laquo;</span>
+                        </a>
+                             <?php for($i=1; $i <= $sotrang; $i++): ?>
+                                 <a class="page-link pagi <?php echo ($i == $p) ? 'active' : '' ?>" href="?page=<?php echo $i ;?>"><?php echo $i; ?></a></li>
+                                 <?php endfor ?>
+                        <a class="page-link pagi nutnext" href="" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                        
                     </ul>
                 </nav>
              
