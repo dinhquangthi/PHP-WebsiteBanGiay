f35c18ea2362129f05a09ccf00ed1855f7443773
Quang Thi <dinhquangthi195@gmail.com>
2019-11-23 11:30:01 +0700

    add filter table - admin

--- admin/modules/order/index.php
+++ admin/modules/order/index.php
@@ -55,12 +55,11 @@ if (isset($orderInfo['page'])) {
                 <?php require_once __DIR__ . "/../../../partials/notification.php"; ?>
             </div>
         </div>
-  
-        <div class="row">
 
+        <div class="row">
             <div class="col-lg-12">
                 <div class="table-responsive">
-                    <table class="table table-bordered table-hover">
+                    <table id="sorting" class="table table-bordered table-hover">
                         <thead>
                             <tr>
                                 <th style="width:11%">Code Orders</th>
@@ -75,35 +74,39 @@ if (isset($orderInfo['page'])) {
                         <tbody>
                             <?php $stt = 1;
                             foreach ($orderInfo as $key => $val) : ?>
-                          
-                                    <tr>
-                                        <td>
-                                            <?php echo $val['id_order_user'] ?></td>
-                                        <td><?php echo $val['nameuser'] ?></td>
-                                        <td>
-                                            <ul>
-                                                <li>Tên sản phẩm : <?php echo "<strong>{$val['productOrder']}</strong>" ?></li>
-                                                <li>Size <?php echo $val['sizeOrder'] ?></li>
-                                                <li>Số lượng: <?php echo $val['quantityOrder'] ?><br>---------------------------------------------</li>
-                                                <li>Giá đơn hàng: <?php echo formatPrice($val['priceOrder']) ?> </li>
-                                                <li>Địa chỉ giao hàng: <?php echo $val['addOrder'] ?> </li>
-                                                <li>Ghi chú: <?php echo $val['noteOrder'] ?> </li>
-                                                <li>Ngày đặt hàng: <?php echo $val['created_at'] ?> </li>
 
-                                            </ul>
-                                        </td>
-                                        <td><?php echo $val['phoneusers'] ?></td>
+                            <tr>
+                                <td>
+                                    <?php echo $val['id_order_user'] ?></td>
+                                <td><?php echo $val['nameuser'] ?></td>
+                                <td>
+                                    <ul>
+                                        <li>Tên sản phẩm : <?php echo "<strong>{$val['productOrder']}</strong>" ?></li>
+                                        <li>Size <?php echo $val['sizeOrder'] ?></li>
+                                        <li>Số lượng:
+                                            <?php echo $val['quantityOrder'] ?><br>---------------------------------------------
+                                        </li>
+                                        <li>Giá đơn hàng: <?php echo formatPrice($val['priceOrder']) ?> </li>
+                                        <li>Địa chỉ giao hàng: <?php echo $val['addOrder'] ?> </li>
+                                        <li>Ghi chú: <?php echo $val['noteOrder'] ?> </li>
+                                        <li>Ngày đặt hàng: <?php echo $val['created_at'] ?> </li>
 
-                                        <td>
-                                            <a id="checkStt" href="status.php?id=<?php echo $val['id'] ?>" class="btn btn-xs <?php echo $val['status'] == 0 ? 'btn-warning' : 'btn-success disabled' ?>">
-                                                <?php echo $val['status'] == 0 ? 'Chưa xử lý' : 'Đã xử lý' ?></a>
+                                    </ul>
+                                </td>
+                                <td><?php echo $val['phoneusers'] ?></td>
 
-                                        </td>
+                                <td>
+                                    <a id="checkStt" href="status.php?id=<?php echo $val['id'] ?>"
+                                        class="btn btn-xs <?php echo $val['status'] == 0 ? 'btn-warning' : 'btn-success disabled' ?>">
+                                        <?php echo $val['status'] == 0 ? 'Chưa xử lý' : 'Đã xử lý' ?></a>
 
-                                        <td>
-                                            <a class="btn btn-danger" href="delete.php?id=<?php echo $val['id'] ?>"><i class="far fa-trash-alt"></i>Xóa</a>
-                                        </td>
-                                    </tr>
+                                </td>
+
+                                <td>
+                                    <a class="btn btn-danger" href="delete.php?id=<?php echo $val['id'] ?>"><i
+                                            class="far fa-trash-alt"></i>Xóa</a>
+                                </td>
+                            </tr>
 
 
                             <?php $stt++;
@@ -120,16 +123,16 @@ if (isset($orderInfo['page'])) {
                                     </a>
                                 </li>
                                 <?php for ($i = 1; $i <= $sotrang; $i++) : ?>
-                                    <?php
+                                <?php
                                         if (isset($_GET['page'])) {
                                             $p = $_GET['page'];
                                         } else {
                                             $p = 1;
                                         }
                                         ?>
-                                    <li class="<?php echo ($i == $p) ? 'active' : '' ?> ">
-                                        <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
-                                    </li>
+                                <li class="<?php echo ($i == $p) ? 'active' : '' ?> ">
+                                    <a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                </li>
                                 <?php endfor; ?>
                                 <li class="page-item">
                                     <a class="page-link" href="#" aria-label="Next">
